f7629928abed5b2e4babd7f9ae53d7d129524a0c
Paige Cote <pmcote1@gmail.com>
2016-05-05 10:59:16 -0400

    working on auth

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 // ...local
 var index = require('./routes/index.js');
+var configAuth = require('./auth.js');
 
 
 /* CONNECT TO MONGOOSE */
@@ -25,17 +27,92 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(passport.initialize());
+
+module.exports = function(passport) {
+
+    // used to serialize the user for the session
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    // used to deserialize the user
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    // code for login (use('local-login', new LocalStategy))
+    // code for signup (use('local-signup', new LocalStategy))
+    // code for facebook (use('facebook', new FacebookStrategy))
+    // code for twitter (use('twitter', new TwitterStrategy))
+
+    // =========================================================================
+    // GOOGLE ==================================================================
+    // =========================================================================
+    passport.use(new GoogleStrategy({
+
+        clientID        : configAuth.clientID,
+        clientSecret    : configAuth.clientSecret,
+        callbackURL     : configAuth.callbackURL,
+
+    },
+    function(token, refreshToken, profile, done) {
+
+        // make the code asynchronous
+        // User.findOne won't fire until we have all our data back from Google
+        process.nextTick(function() {
+
+            // try to find the user based on their google id
+            User.findOne({ 'google.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+
+                    // if a user is found, log them in
+                    return done(null, user);
+                } else {
+                    // if the user isnt in our database, create a new user
+                    var newUser          = new User();
+
+                    // set all of the relevant information
+                    newUser.google.id    = profile.id;
+                    newUser.google.token = token;
+                    newUser.google.name  = profile.displayName;
+                    newUser.google.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+
+    }));
+
+};
 
 
 /* ROUTING */
 app.get('/', index.video);
-app.get('/getComments', index.getComments);
-app.get('/getVideos', index.getVideos);
-app.get('/:videoid', index.byVideo);
+app.get('/auth', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+// the callback after google has authenticated the user
+app.get('/auth/callback',
+        passport.authenticate('google', {
+                successRedirect : '/',
+                failureRedirect : '/'
+        }));
 
 app.post('/newVideo', index.newVideo);
 app.post('/newComment', index.newComment);
 app.post('/newReply', index.newReply);
+
 
 app.listen(process.env.PORT || 3000);
 console.log("Running on port 3000");
